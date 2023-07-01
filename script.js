d5a1283a2f4be8c2f3499978e383250a623fa487
Sean <sdrowan93@gmail.com>
2023-07-01 13:39:43 -0500

    removed unneeded elements

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const currentDay = $('#currentDay');
 const saveBtn =$('saveBtn')
 let currentTime =dayjs().hour()
-var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-
+var rightNow = dayjs().format('MMMM DD, YYYY [at] hh:mm a');
 let textArea =$('textarea')
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -17,12 +16,11 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   $('.saveBtn').on ("click", function (){
-   var textI = $(this).siblings('.description').val();
+    var textI = $(this).siblings('.description').val();
    var textTime = $(this).parent().attr('id')
-  localStorage.setItem(textI, textTime)
-   console.log(textI, textTime)
+  localStorage.setItem(textTime,textI)
   } );
- 
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. 
@@ -34,37 +32,34 @@ $(function () {
   $(".time-block").each(function () {
 
     var hourBlock = parseInt($(this).attr("id").split("hour-")[1]);
-    console.log(hourBlock)
     if (currentTime > hourBlock) {
-      $(this).removeClass("future");
-      $(this).removeClass("present");
       $(this).addClass("past");
     } else if (currentTime == hourBlock) {
-      $(this).removeClass("past");
-      $(this).removeClass("future");
       $(this).addClass("present");
     } else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
       $(this).addClass("future");
     };
   });
-console.log(currentTime)
 
-  
-  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements.
   // HINT: How can the id
   // attribute of each time-block be used to do this?
   //
- 
 
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'))
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'))
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'))
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'))
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'))
 
   // TODO: Add code to display the current date in the header of the page.
-  function displayTime() {
-    currentDay.text(rightNow);
+  
+    $(currentDay.text(rightNow));
     //setInterval(displayTime,1000)
-  }
-  displayTime()
+  
 });
